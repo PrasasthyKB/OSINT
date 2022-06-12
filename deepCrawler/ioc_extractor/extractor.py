015a84ff57b18c51f96ed3154a5e3d5815639c77
dark-0ne <khoshkholgh.danial@gmail.com>
2022-06-12 19:36:51 +0300

    Add url extraction
    Still need to add kafka support

--- deepCrawler/ioc_extractor/extractor.py
+++ deepCrawler/ioc_extractor/extractor.py
@@ -1,5 +1,7 @@
 import json
 import ipaddress
+import itertools
+from urllib.parse import urlparse
 
 import requests
 
@@ -46,8 +48,14 @@ for yara in iocextract.extract_yara_rules(text):
 # Extract emails
 for email in iocextract.extract_emails(text, refang=True):
     iocs.append({'email': {"ioc": email}})
+
 # Extract IPs
-for ip_addr in iocextract.extract_ips(text, refang=True):
+for ip_addr in iocextract.extract_ips(text):
+
+    # Skip if ip is not defanged
+    if ip_addr == iocextract.refang_ipv4(ip_addr):
+        continue
+    ip_addr = iocextract.refang_ipv4(ip_addr)
     ip_addr.replace('[', '').replace(']', '').split(
         '/')[0].split(':')[0].split(' ')[0]
     try:
@@ -62,4 +70,47 @@ for ip_addr in iocextract.extract_ips(text, refang=True):
             continue
     iocs.append({'ip': {"ver": ip_ver, "ioc": str(ip_addr)}})
 
-print(iocs)
+# Extract URLs
+urls = itertools.chain(
+    iocextract.extract_unencoded_urls(text),
+    iocextract.extract_encoded_urls(text, refang=True),
+)
+for url in urls:
+    # Skip if url is not defanged
+    if url == iocextract.refang_url(url):
+        continue
+    url = iocextract.refang_url(url)
+    possible_ip = urlparse(url).netloc.split(':')[0].replace(
+        '[', '').replace(']', '').replace(',', '.')
+
+    # Skip if url is ip
+    try:
+        ipaddress.IPv4Address(possible_ip)
+        continue
+    except ValueError:
+        try:
+            ipaddress.IPv6Address(possible_ip)
+            continue
+        except ValueError:
+            pass
+
+    try:
+        url_domain = urlparse(url).netloc.split(':')[0]
+    # Trouble parsing url, just skip
+    except ValueError:
+        continue
+    # Skip if url is in the same domain as current page
+    if url_domain == urlparse(r.url).netloc:
+        print(f"Found url with same domain: {url}")
+        continue
+    iocs.append({'url': {"domain": url_domain, "ioc": url}})
+
+# Remove duplicates
+unique_iocs_set = set()
+unique_iocs_list = []
+for record in iocs:
+    if list(record.values())[0]['ioc'] not in unique_iocs_set:
+        unique_iocs_set.add(list(record.values())[0]['ioc'])
+        unique_iocs_list.append(record)
+
+print(unique_iocs_list)
