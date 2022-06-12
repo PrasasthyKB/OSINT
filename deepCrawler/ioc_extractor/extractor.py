9351c7e433de4bc921819111603bce26c22cfe3a
dark-0ne <khoshkholgh.danial@gmail.com>
2022-06-12 03:08:37 +0000

    Add initial IoC extractor
    Only extracts YARA, hashes,IPs and email. Need to add URL extractor
    and kafka support.

---
+++ deepCrawler/ioc_extractor/extractor.py
@@ -0,0 +1,65 @@
+import json
+import ipaddress
+
+import requests
+
+import html2text
+
+from kafka import KafkaConsumer, KafkaProducer
+import iocextract
+
+CONSUME_TOPIC_NAME = "IoCRelevants"
+PRODUCE_TOPIC_NAME = "DwebIoCs"
+CONSUMER_GROUP = "DwebClassifier"
+KAFKA_SERVER = "128.214.254.195:9093"
+
+consumer = KafkaConsumer(CONSUME_TOPIC_NAME, bootstrap_servers=KAFKA_SERVER, value_deserializer=lambda m: json.loads(
+    m.decode('utf-8')), auto_offset_reset='earliest')
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+
+r = requests.get(
+    "https://securelist.com/moonbounce-the-dark-side-of-uefi-firmware/105468/")
+
+text = html2text.html2text(r.text)
+
+iocs = []
+
+# Extract hashes
+for hash_ in iocextract.extract_hashes(text):
+
+    hash_type = 'misc.'
+    if len(hash_) == 32:
+        hash_type = "MD5"
+    elif len(hash_) == 40:
+        hash_type = "SHA1"
+    elif len(hash_) == 64:
+        hash_type = "SHA256"
+    elif len(hash_) == 128:
+        hash_type = "SHA512"
+
+    iocs.append({'hash': {"hash_type": hash_type, "ioc": hash_}})
+
+# Extract YARA rules
+for yara in iocextract.extract_yara_rules(text):
+    iocs.append({'yara': {"ioc": yara}})
+
+# Extract emails
+for email in iocextract.extract_emails(text, refang=True):
+    iocs.append({'email': {"ioc": email}})
+# Extract IPs
+for ip_addr in iocextract.extract_ips(text, refang=True):
+    ip_addr.replace('[', '').replace(']', '').split(
+        '/')[0].split(':')[0].split(' ')[0]
+    try:
+        ip_addr = ipaddress.IPv4Address(ip_addr)
+        ip_ver = "IPv4"
+    except ValueError:
+        try:
+            ip_addr = ipaddress.IPv6Address(ip_addr)
+            ip_ver = "IPv6"
+        except ValueError:
+            print(f"ip {ip_addr} format not recognized!")
+            continue
+    iocs.append({'ip': {"ver": ip_ver, "ioc": str(ip_addr)}})
+
+print(iocs)
