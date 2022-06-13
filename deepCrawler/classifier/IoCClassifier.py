aea7062e8efa514b97c47383900168c015145197
dark-0ne <khoshkholgh.danial@gmail.com>
2022-06-13 15:12:10 +0300

    Add AWS S3 to IoCClassifier
    Classifier will now download model from AWS S3

--- deepCrawler/classifier/IoCClassifier.py
+++ deepCrawler/classifier/IoCClassifier.py
@@ -1,4 +1,5 @@
 import json
+import configparser
 
 import torch
 from torch import nn
@@ -6,13 +7,18 @@ from torch import nn
 from transformers import BertTokenizer, BertModel, BertConfig
 
 from kafka import KafkaConsumer, KafkaProducer
+import boto3
 
 CONSUME_TOPIC_NAME = "IoCPagesTest"
 PRODUCE_TOPIC_NAME = "IoCRelevants"
-CONSUMER_GROUP = "DwebClassifier"
+CONSUMER_GROUP = "DwebClassifier1"
 KAFKA_SERVER = "128.214.254.195:9093"
 
-PATH_TO_MODEL = "model_checkpoints/multi-512-20-balanced.pt"
+AWS_CREDS_PATH = "/run/secrets/aws_creds"
+MODEL_PATH = "model_checkpoints/multi-512-20-balanced.pt"
+
+aws_configs = configparser.ConfigParser()
+aws_configs.read(AWS_CREDS_PATH)
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
 
@@ -40,26 +46,40 @@ class BertClassifier(nn.Module):
 
 
 if __name__ == "__main__":
-    consumer = KafkaConsumer(CONSUME_TOPIC_NAME, bootstrap_servers=KAFKA_SERVER, value_deserializer=lambda m: json.loads(
-        m.decode('utf-8')), auto_offset_reset='earliest', group_id=CONSUMER_GROUP)
+    consumer = KafkaConsumer(
+        CONSUME_TOPIC_NAME, bootstrap_servers=KAFKA_SERVER,
+        value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+        auto_offset_reset='earliest', group_id=CONSUMER_GROUP)
     producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
 
+    s3 = boto3.client(
+        's3',
+        region_name='eu-central-1',
+        aws_access_key_id=aws_configs["AWS_CREDENTIALS"]["ACCESS_KEY_ID"],
+        aws_secret_access_key=aws_configs["AWS_CREDENTIALS"]["ACCESS_TOKEN"]
+    )
+    with open(MODEL_PATH, 'wb') as f:
+        s3.download_fileobj('thor-infra',
+                            'crawl-models/multi-512-20-balanced.pt', f)
+
     if use_cuda:
         model = model.cuda()
 
     model = BertClassifier()
-    checkpoint = torch.load(PATH_TO_MODEL, map_location=device)
+    checkpoint = torch.load(MODEL_PATH, map_location=device)
     model.load_state_dict(checkpoint['model_state_dict'])
     model.eval()
 
     with torch.no_grad():
         for data in consumer:
+            print("Got data")
             text = data.value["text"]
             tokenized = tokenizer(
-                text, padding='max_length', max_length=512, truncation=True, return_tensors="pt")
+                text, padding='max_length', max_length=512, truncation=True,
+                return_tensors="pt")
             mask = tokenized['attention_mask'].to(device)
             input_id = tokenized['input_ids'].squeeze(1).to(device)
 
