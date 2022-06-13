140a869906bc3b9d6ed2e85dc1fd878b0daa96f2
dark-0ne <khoshkholgh.danial@gmail.com>
2022-06-13 07:15:42 +0000

    Add kafka to extracor (not tested)

--- deepCrawler/ioc_extractor/extractor.py
+++ deepCrawler/ioc_extractor/extractor.py
@@ -3,10 +3,6 @@ import ipaddress
 import itertools
 from urllib.parse import urlparse
 
-import requests
-
-import html2text
-
 from kafka import KafkaConsumer, KafkaProducer
 import iocextract
 
@@ -19,98 +15,101 @@ consumer = KafkaConsumer(CONSUME_TOPIC_NAME, bootstrap_servers=KAFKA_SERVER, val
     m.decode('utf-8')), auto_offset_reset='earliest')
 producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
-r = requests.get(
-    "https://securelist.com/moonbounce-the-dark-side-of-uefi-firmware/105468/")
+for data in consumer:
+    text = data.value["text"]
+    iocs = []
 
-text = html2text.html2text(r.text)
+    # Extract hashes
+    for hash_ in iocextract.extract_hashes(text):
 
-iocs = []
+        hash_type = 'misc.'
+        if len(hash_) == 32:
+            hash_type = "MD5"
+        elif len(hash_) == 40:
+            hash_type = "SHA1"
+        elif len(hash_) == 64:
+            hash_type = "SHA256"
+        elif len(hash_) == 128:
+            hash_type = "SHA512"
 
-# Extract hashes
-for hash_ in iocextract.extract_hashes(text):
+        iocs.append({'hash': {"hash_type": hash_type, "ioc": hash_}})
 
-    hash_type = 'misc.'
-    if len(hash_) == 32:
-        hash_type = "MD5"
-    elif len(hash_) == 40:
-        hash_type = "SHA1"
-    elif len(hash_) == 64:
-        hash_type = "SHA256"
-    elif len(hash_) == 128:
-        hash_type = "SHA512"
+    # Extract YARA rules
+    for yara in iocextract.extract_yara_rules(text):
+        iocs.append({'yara': {"ioc": yara}})
 
-    iocs.append({'hash': {"hash_type": hash_type, "ioc": hash_}})
+    # Extract emails
+    for email in iocextract.extract_emails(text, refang=True):
+        iocs.append({'email': {"ioc": email}})
 
-# Extract YARA rules
-for yara in iocextract.extract_yara_rules(text):
-    iocs.append({'yara': {"ioc": yara}})
+    # Extract IPs
+    for ip_addr in iocextract.extract_ips(text):
 
-# Extract emails
-for email in iocextract.extract_emails(text, refang=True):
-    iocs.append({'email': {"ioc": email}})
-
-# Extract IPs
-for ip_addr in iocextract.extract_ips(text):
-
-    # Skip if ip is not defanged
-    if ip_addr == iocextract.refang_ipv4(ip_addr):
-        continue
-    ip_addr = iocextract.refang_ipv4(ip_addr)
-    ip_addr.replace('[', '').replace(']', '').split(
-        '/')[0].split(':')[0].split(' ')[0]
-    try:
-        ip_addr = ipaddress.IPv4Address(ip_addr)
-        ip_ver = "IPv4"
-    except ValueError:
-        try:
-            ip_addr = ipaddress.IPv6Address(ip_addr)
-            ip_ver = "IPv6"
-        except ValueError:
-            print(f"ip {ip_addr} format not recognized!")
+        # Skip if ip is not defanged
+        if ip_addr == iocextract.refang_ipv4(ip_addr):
             continue
-    iocs.append({'ip': {"ver": ip_ver, "ioc": str(ip_addr)}})
-
-# Extract URLs
-urls = itertools.chain(
-    iocextract.extract_unencoded_urls(text),
-    iocextract.extract_encoded_urls(text, refang=True),
-)
-for url in urls:
-    # Skip if url is not defanged
-    if url == iocextract.refang_url(url):
-        continue
-    url = iocextract.refang_url(url)
-    possible_ip = urlparse(url).netloc.split(':')[0].replace(
-        '[', '').replace(']', '').replace(',', '.')
-
-    # Skip if url is ip
-    try:
-        ipaddress.IPv4Address(possible_ip)
-        continue
-    except ValueError:
+        ip_addr = iocextract.refang_ipv4(ip_addr)
+        ip_addr.replace('[', '').replace(']', '').split(
+            '/')[0].split(':')[0].split(' ')[0]
         try:
-            ipaddress.IPv6Address(possible_ip)
+            ip_addr = ipaddress.IPv4Address(ip_addr)
+            ip_ver = "IPv4"
+        except ValueError:
+            try:
+                ip_addr = ipaddress.IPv6Address(ip_addr)
+                ip_ver = "IPv6"
+            except ValueError:
+                print(f"ip {ip_addr} format not recognized!")
+                continue
+        iocs.append({'ip': {"ver": ip_ver, "ioc": str(ip_addr)}})
+
+    # Extract URLs
+    urls = itertools.chain(
+        iocextract.extract_unencoded_urls(text),
+        iocextract.extract_encoded_urls(text, refang=True),
+    )
+    for url in urls:
+        # Skip if url is not defanged
+        if url == iocextract.refang_url(url):
+            continue
+        url = iocextract.refang_url(url)
+        possible_ip = urlparse(url).netloc.split(':')[0].replace(
+            '[', '').replace(']', '').replace(',', '.')
+
+        # Skip if url is ip
+        try:
+            ipaddress.IPv4Address(possible_ip)
             continue
         except ValueError:
-            pass
+            try:
+                ipaddress.IPv6Address(possible_ip)
+                continue
+            except ValueError:
+                pass
 
-    try:
-        url_domain = urlparse(url).netloc.split(':')[0]
-    # Trouble parsing url, just skip
-    except ValueError:
-        continue
-    # Skip if url is in the same domain as current page
-    if url_domain == urlparse(r.url).netloc:
-        print(f"Found url with same domain: {url}")
-        continue
-    iocs.append({'url': {"domain": url_domain, "ioc": url}})
+        try:
+            url_domain = urlparse(url).netloc.split(':')[0]
+        # Trouble parsing url, just skip
+        except ValueError:
+            continue
+        # Skip if url is in the same domain as current page
+        if url_domain == urlparse(r.url).netloc:
+            print(f"Found url with same domain: {url}")
+            continue
+        iocs.append({'url': {"domain": url_domain, "ioc": url}})
 
-# Remove duplicates
-unique_iocs_set = set()
-unique_iocs_list = []
-for record in iocs:
-    if list(record.values())[0]['ioc'] not in unique_iocs_set:
-        unique_iocs_set.add(list(record.values())[0]['ioc'])
-        unique_iocs_list.append(record)
+    # Remove duplicates
+    unique_iocs_set = set()
+    unique_iocs_list = []
+    for record in iocs:
+        if list(record.values())[0]['ioc'] not in unique_iocs_set:
+            unique_iocs_set.add(list(record.values())[0]['ioc'])
+            unique_iocs_list.append(record)
 
-print(unique_iocs_list)
+    payload = {key: data.value[key]
+               for key in ["url", "spider_name", "date_inserted"]}
+    payload["iocs"] = unique_iocs_list
+    json_payload = json.dumps(payload)
+    json_payload = str.encode(json_payload)
+    producer.send(PRODUCE_TOPIC_NAME, json_payload)
+    producer.flush()
